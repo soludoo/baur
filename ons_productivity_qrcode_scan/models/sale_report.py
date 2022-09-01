b26e4f53c60034104af76632752576ca1fcf8737
baur-insektenschutz <85956165+baur-insektenschutz@users.noreply.github.com>
2022-09-01 19:47:07 +0530

    Update sale_report.py

--- ons_productivity_qrcode_scan/models/sale_report.py
+++ ons_productivity_qrcode_scan/models/sale_report.py
@@ -6,6 +6,8 @@ from odoo import models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
+    execution = fields.Char(string="Execution", readonly=True)
+
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['execution'] = ", s.execution as execution"
         groupby += ', s.execution'
